545b0fa3dac12db474d2aa88b1c4ea8f8dff434b
Reid Bishop <rdbishop@Reid-Bishops-MacBook-Pro.local>
2013-12-02 11:30:06 -0500

    Added one file for all javascript. Just include at the top or bottom or every page.

---
+++ includeJS.php
@@ -0,0 +1,20 @@
+<html>
+	<head>
+		<script type="text/javascript" src="dist/js/jquery-2.0.3.js"></script>
+		<script type="text/javascript" src="dist/js/bootstrap-bootstrap.js"></script>
+		<script type="text/javascript" src="dist/js/bootstrap-alert.js"></script>
+		<script type="text/javascript" src="dist/js/bootstrap-button.js"></script>
+		<script type="text/javascript" src="dist/js/bootstrap-carousel.js"></script>
+		<script type="text/javascript" src="dist/js/bootstrap-collapse.js"></script>
+		<script type="text/javascript" src="dist/js/bootstrap-dropdown.js"></script>
+		<script type="text/javascript" src="dist/js/bootstrap-modal.js"></script>
+		<script type="text/javascript" src="dist/js/bootstrap-popover.js"></script>
+		<script type="text/javascript" src="dist/js/bootstrap-scrollspy.js"></script>
+		<script type="text/javascript" src="dist/js/bootstrap-tab.js"></script>
+		<script type="text/javascript" src="dist/js/bootstrap-tooltip.js"></script>
+		<script type="text/javascript" src="dist/js/bootstrap-transition.js"></script>
+		<script type="text/javascript" src="dist/js/bootstrap-typeahead.js"></script>
+		<script type="text/javascript" src="dist/js/holder.js"></script>
+		<script type="text/javascript" src="dist/js/moment.js"></script>
+	</head>
+</html>
