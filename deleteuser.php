f6513eb90ba202085036c11d4d94cc89c222f249
dangertwo <dhoffman19@live.com>
2014-04-25 20:30:47 -0400

    Delete now works with editted timestamps
    Damn foreign keys

--- deleteuser.php
+++ deleteuser.php
@@ -10,8 +10,18 @@ $id = $_GET['id'];
 $query="Delete From UserCourses where UserID=".$id;
 $query2="Delete from AccountLevel where UserID=".$id;
 $query3="Delete from InstructorBio where ID=".$id;
-$query4="Delete From UserAccounts Where id=".$id;
-$query5="Delete From TimeClock where UserID=".$id;
+$queryc= "Select StampID from TimeClock where UserID=".$id;
+$response = mysql_query($queryc, $link);
+while($results = mysql_fetch_array($response)) {
+	$queryt = "Delete from TimeClockOverride where StampID =".$results['StampID'];
+	mysql_query($queryt, $link);
+}
+
+$queryg="Delete from TimeClockOverride where UserID=".$id;
+$query4="Delete From TimeClock where UserID=".$id;
+$query5="Delete From UserAccounts Where id=".$id;
+
+mysql_query($queryg, $link);
 mysql_query($query, $link);
 mysql_query($query2, $link);
 mysql_query($query3, $link);
